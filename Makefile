5c149120756f5cce89f4d87fdaef289b1049556f
christinajli <28367784+christinajli@users.noreply.github.com>
2023-03-18 12:57:48 -0400

    Create unit tests for morseTree class

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 CFLAGS = -Wall -std=c++17
+
 output: main.o morseTree.o
 	g++ $(CFLAGS) main.o morseTree.o -o output
 
@@ -8,5 +9,11 @@ main.o: main.cpp
 morseTree.o: morseTree.cpp morseTree.hpp
 	g++ -c $(CFLAGS) morseTree.cpp
 
+test_tree: test_morseTree.o morseTree.o
+	g++ $(CFLAGS) test_morseTree.o morseTree.o -o test_tree
+
+test_morseTree.o: test_morseTree.cpp
+	g++ -c $(CFLAGS) test_morseTree.cpp
+
 clean:
-	rm *.o output
+	rm *.o output test_tree
---
+++ test_morseTree.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <assert.h>
+#include "morseTree.hpp"
+using namespace std;
+
+void test_buildTree() {
+    string order = "abcdefg";
+    cout << "Testing  buildTree() with order: " << order << endl;
+
+    morseTree tree;
+    tree.buildTree(order);
+    treeNode* n = tree.getRoot();
+
+    assert(n->character == '*');
+
+    assert(n->leftNode->character == 'a');
+    assert(n->leftNode->code == ".");
+
+    assert(n->rightNode->character == 'b');
+    assert(n->rightNode->code == "-");
+
+    assert(n->leftNode->leftNode->leftNode->character == 'g');
+    assert(n->leftNode->leftNode->leftNode->code == "...");
+
+    cout << "\t buildTree() tests passed" << endl;
+    cout << endl;
+}
+
+void test_decode() {
+    string order = "mnopqrstuv";
+    cout << "Testing  decode() with order: " << order << endl;
+
+    morseTree tree;
+    tree.buildTree(order);
+
+    assert(tree.decode(".") == 'm');
+    assert(tree.decode("-") == 'n');
+
+    assert(tree.decode("..") == 'o');
+    assert(tree.decode("--") == 'r');
+
+    assert(tree.decode("--.") == '\0');
+    assert(tree.decode("---") == '\0');
+
+    cout << "\t decode() tests passed" << endl;
+    cout << endl;
+}
+
+void test_getCode() {
+    string order = "wxyz123";
+    cout << "Testing  test_getCode() with order: " << order << endl;
+
+    morseTree tree;
+    tree.buildTree(order);
+
+    assert(tree.getCode('w') == ".");
+    assert(tree.getCode('x') == "-");
+
+    assert(tree.getCode('1') == "-.");
+    assert(tree.getCode('2') == "--");
+
+    assert(tree.getCode('4') == "?");
+    assert(tree.getCode('5') == "?");
+
+    cout << "\t test_getCode() tests passed" << endl;
+    cout << endl;
+}
+
+int main(int argc, char** argv) {
+    cout << "Running unit tests for morseTree class" << endl;
+    cout << endl;
+
+    test_buildTree();
+    test_decode();
+    test_getCode();
+
+    return 0;
+}
